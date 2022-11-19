5c514e04018be8bde01508a87e6a387634eb7050
Thomas Newcomb <volunshade@gmail.com>
2022-11-19 14:59:00 -0800

    worked further on class Deck, updated class Card, updated Main.

--- code.py
+++ code.py
@@ -11,15 +11,20 @@ class Suit:
         self.name = name
         self.symbol = symbol
 
+    def __str__(self) -> str:
+        """String representation of a suit."""
+
+        return f"Suit(name={self.name}, symbol={self.symbol})"
+
     @property
     def color(self) -> str:
         """Determine the color of a suit."""
 
         color = None
 
-        if self.name in ["heart", "diamond"]
+        if self.name in ["heart", "diamond"]:
             color = "red"
-        elif self.name in ["club", "spade"]
+        elif self.name in ["club", "spade"]:
             color = "black"
         else:
             raise Exception("Invalid suit name.")
@@ -30,13 +35,18 @@ class Suit:
 class Card:
     """Representation of a card."""
 
-    def __init__(self, name: str, value: str, suit: Suit):
+    def __init__(self, name: str, value: int, suit: Suit) -> None:
         """Create a new instance."""
 
         self.name = name
         self.value = value
         self.suit = suit
-  
+
+    def __str__(self) -> str:
+        """String representation of a card."""
+
+        return f"Card(name={self.name}, value={self.value}, suit={self.suit})"
+
 
 class Deck:
     """Representation of a deck."""
@@ -44,30 +54,55 @@ class Deck:
     def __init__(self):
         """Create a new instance."""
 
-        self.cardlist = []
+        self.cards = []
 
-        cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
-        suits = ["heart", "diamond", "spade", "club"]
-        for card in self.cards:
-            for suit in self.suits:
-                self.cardlist.append(Card(value,suit))
-
+        suits = [
+            ("heart", u"\u2665"),
+            ("spade", u"\u2660"),
+            ("club", u"\u2663"),
+            ("diamond", u"\u2666")
+        ]
+        for _suit in suits:
+            suit = Suit(*_suit)
+            cards = [
+                ("ace", 1, suit),
+                ("two", 2, suit),
+                ("three", 3, suit),
+                ("four", 4, suit),
+                ("five", 5, suit),
+                ("six", 6, suit),
+                ("seven", 7, suit),
+                ("eight", 8, suit),
+                ("nine", 9, suit),
+                ("ten", 10, suit),
+                ("jack", 10, suit),
+                ("queen", 10, suit),
+                ("king", 10, suit)
+            ]
+            for _card in cards:
+                card = Card(*_card)
+                self.cards.append(card)
     def shuffle(self):
         """Shuffles the deck of cards."""
 
-        self.shuffled = random.shuffle()
+        random.shuffle(self.cards)
 
     def deal_card(self):
         """Deal a card from the deck."""
 
-        pass
+        return self.cards.pop()
 
 
 def main():
     """This will shuffle and deal a card from the deck."""
 
-    pass
+    deck = Deck()
+    deck.shuffle()
+    player_card = deck.deal_card()
+    print(player_card)
 
+    for card in deck.cards:
+        print(card)
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
